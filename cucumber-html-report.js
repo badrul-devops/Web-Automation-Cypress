19f7b37307d33f663beec09648e5e8610be63490
badrul-devops <badrul.qups@gmail.com>
2023-11-13 15:51:16 +0600

    code

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,5 +1,6 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
+  
   jsonDir: "jsonlogs", // ** Path of .json file **//
   reportPath: "./reports/cucumber-htmlreport.html",
   metadata: {
